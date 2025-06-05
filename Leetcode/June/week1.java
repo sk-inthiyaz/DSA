2660b058e25be260700f3d5488adda271e014964
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-05 13:36:51 +0530

    142. Linked List Cycle II

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -5,6 +5,7 @@
 // 141. Linked List Cycle [02-06-2025]
 // 234. Palindrome Linked List[03-06-2025]
 // 83. Remove Duplicates from Sorted List [04-06-2025]
+// 142. Linked List Cycle II [05-06-2025]
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -382,6 +383,71 @@ public ListNode deleteDuplicates(ListNode head) {
         }
         return head;
     }
+
+
+//142. Linked List Cycle II
+/*
+Medium
+Topics
+premium lock icon
+Companies
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ */
+
+
+public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                while(head!=slow){
+                    head=head.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+            
+        }
+        return null;
+    }
+
 
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
