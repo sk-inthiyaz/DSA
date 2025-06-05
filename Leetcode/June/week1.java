2ad74ac5ce11fb97b84f9dff6236c11da670b48a
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-05 23:27:41 +0530

    342. Power of Four

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -11,6 +11,7 @@
 // 328. Odd Even Linked List        
 // 19. Remove Nth Node From End of List 
 // 326. Power of Three
+// 342. Power of Four
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -639,6 +640,45 @@ Explanation: There is no x where 3x = (-1).
         if(n<=0 || n%3!=0) return false;
         return isPowerOfThree(n/3);
     }
+
+
+/*
+342. Power of Four
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+Example 1:
+
+Input: n = 16
+Output: true
+Example 2:
+
+Input: n = 5
+Output: false
+Example 3:
+
+Input: n = 1
+Output: true
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+ */
+public boolean isPowerOfFour(int n) {
+        if(n==1) return true;
+        if(n<=0 || n%4!=0) return false;
+        return isPowerOfFour(n/4);
+    }
+
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
 
