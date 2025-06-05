a7bc00faa53e6e144c82bb7b3f0d5a3a6e33d136
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-05 15:44:51 +0530

    328. Odd Even Linked List

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -7,6 +7,7 @@
 // 83. Remove Duplicates from Sorted List [04-06-2025]
 // 142. Linked List Cycle II                [05-06-2025]
 // Find length of Loop (Greek for greeks)       ^
+// 328. Odd Even Linked List        
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -495,6 +496,58 @@ will be connected to the last node in the LinkedList.
     //     return count;
         
     // }
+
+
+
+
+// 328. Odd Even Linked List
+/*
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+Example 2:
+
+
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
+ 
+
+Constraints:
+
+The number of nodes in the linked list is in the range [0, 104].
+-106 <= Node.val <= 106
+ */
+public ListNode oddEvenList(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenhead = even;
+        while(even!=null && even.next!=null){
+            odd.next = odd.next.next;
+            odd = odd.next;
+
+            even.next = even.next.next;
+            even = even.next;
+        }
+
+        odd.next = evenhead;
+        return head;
+    }
+
 
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
