3cd1fe5b351459ed52584192f1de2789e9287cbd
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-05 14:33:20 +0530

    Find length of Loop(GFG)

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -5,7 +5,8 @@
 // 141. Linked List Cycle [02-06-2025]
 // 234. Palindrome Linked List[03-06-2025]
 // 83. Remove Duplicates from Sorted List [04-06-2025]
-// 142. Linked List Cycle II [05-06-2025]
+// 142. Linked List Cycle II                [05-06-2025]
+// Find length of Loop (Greek for greeks)       ^
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -448,6 +449,52 @@ public ListNode detectCycle(ListNode head) {
         return null;
     }
 
+/*
+Find length of Loop
+Difficulty: EasyAccuracy: 44.26%Submissions: 258K+Points: 2Average Time: 30m
+Given the head of a linked list, determine whether the list contains a loop. If a loop is present, return the number of nodes in the loop, otherwise return 0.
+
+Note: 'c' is the position of the node which is the next pointer of the last node of the linkedlist. If c is 0, then there is no loop.
+
+Examples:
+
+Input: head: 1 → 2 → 3 → 4 → 5, c = 2
+Output: 4
+Explanation: There exists a loop in the linked list and the length of the loop is 4.
+
+Input: head: 25 → 14 → 19 → 33 → 10 → 21 → 39 → 90 → 58 → 45, c = 4
+Output: 7
+Explanation: The loop is from 33 to 45. So length of loop is 33 → 10 → 21 → 39 → 90 → 58 → 45 = 7.
+The number 33 is connected to the last node of the linkedlist to form the loop because according to the input the 4th node from the beginning(1 based indexing) 
+will be connected to the last node in the LinkedList.
+
+ */
+
+    // Function to find the length of a loop in the linked list
+    // public int countNodesinLoop(Node head) {
+    //     // code here.
+    //     Node slow = head;
+    //     Node fast = head;
+    //     while(fast!=null && fast.next!=null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //         if(slow == fast){
+    //            return countLoopLength(slow);
+    //         }
+            
+    //     }
+    //     return 0;
+    // }
+    // private int countLoopLength(Node NodeInLoop){
+    //     int count = 1;
+    //     Node temp = NodeInLoop.next;
+    //     while(temp!=NodeInLoop){
+    //         count++;
+    //         temp = temp.next;
+    //     }
+    //     return count;
+        
+    // }
 
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
