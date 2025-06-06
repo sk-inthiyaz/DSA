ff95196b8d6fc71eb1b6c39b9fb608af321d68a5
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-06 15:26:24 +0530

    2095. Delete the Middle Node of a Linked List

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -12,6 +12,8 @@
 // 19. Remove Nth Node From End of List 
 // 326. Power of Three
 // 342. Power of Four
+// [06-06-2025]
+// 2095. Delete the Middle Node of a Linked List
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -678,6 +680,69 @@ public boolean isPowerOfFour(int n) {
         if(n<=0 || n%4!=0) return false;
         return isPowerOfFour(n/4);
     }
+
+// 2095. Delete the Middle Node of a Linked List
+
+    /*
+    You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+
+Example 1:
+
+
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+Example 2:
+
+
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+Example 3:
+
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+1 <= Node.val <= 105
+     */
+
+public ListNode deleteMiddle(ListNode head) {
+        // 1-2-3-4-5 [1-2-4-5] 3
+        // 8-2-1-9 [8-2-9]     1
+        if(head==null || head.next==null){
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev  = null;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        return head;
+    }
+
 
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
