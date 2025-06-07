438165e8f83c399810a8970047ba9baddae8fd97
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-07 15:36:44 +0530

    2. Add Two Numbers

--- Leetcode/June/week1.java
+++ Leetcode/June/week1.java
@@ -14,6 +14,7 @@
 // 342. Power of Four
 // [06-06-2025]
 // 2095. Delete the Middle Node of a Linked List
+// 2. Add Two Numbers
 //Linked list  should be covered
 
 import java.util.ArrayList;
@@ -743,6 +744,64 @@ public ListNode deleteMiddle(ListNode head) {
         return head;
     }
 
+
+/*
+2. Add Two Numbers
+Medium
+Topics
+premium lock icon
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode ptr = result;
+        int carry = 0 ;
+        while(l1!=null || l2!=null){
+            int sum = 0 + carry;
+            if(l1!=null){
+                sum+=l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2 = l2.next;
+            }
+            carry = sum/10;
+            sum = sum%10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
+        }
+        if(carry==1)  ptr.next = new ListNode(1);
+        return result.next;
+
+    }
 
     public static void main(String[] args) {
         // int[] input = {1, 2, 3, 4, 5, 6}; // Change this array to test other cases
