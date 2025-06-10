75bfb96cadcb5a8b9e66179a83a969916e646498
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-10 14:50:12 +0530

    382. Linked List Random Node

--- Leetcode/June/week2.java
+++ Leetcode/June/week2.java
@@ -1,4 +1,5 @@
 //148. Sort List [09-06-2025]
+//382. Linked List Random Node [10-06-25]
 import java.util.*;
 
 public class week2 {
@@ -45,6 +46,72 @@ public class week2 {
         }
         System.out.println();
     }
+
+/*
+382. Linked List Random Node
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Implement the Solution class:
+
+Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+ 
+
+Example 1:
+
+
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+Constraints:
+
+The number of nodes in the linked list will be in the range [1, 104].
+-104 <= Node.val <= 104
+At most 104 calls will be made to getRandom.
+ 
+
+Follow up:
+
+What if the linked list is extremely large and its length is unknown to you?
+Could you solve this efficiently without using extra space?
+ */
+//Time:O(n) 
+    //space:O(1)
+    ArrayList<Integer> arr = new ArrayList<>();
+    //O(n) t
+    //O(n) s
+    public week2(ListNode head) {
+        ListNode temp = head;
+        while(temp!=null){
+            arr.add(temp.val);
+            temp = temp.next;
+        }
+    }
+    // O(1)
+    public int getRandom() {
+        // int n = arr.size();
+        // int random_index = (int)
+        return arr.get((int)(Math.random() * arr.size()));
+    }
+
 
     public static void main(String[] args) {
         // Example: Linked List = [4, 2, 1, 3]
