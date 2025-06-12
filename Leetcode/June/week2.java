9b096834c41dd22d6f02288585ba7a043903c053
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-12 23:31:05 +0530

    3423. Maximum Difference Between Adjacent Elements in a Circular Array

--- Leetcode/June/week2.java
+++ Leetcode/June/week2.java
@@ -1,7 +1,8 @@
 //148. Sort List [09-06-2025]
 //382. Linked List Random Node [10-06-25]
 //20. Valid Parentheses [11-06-2025]
-//1614. Maximum Nesting Depth of the Parentheses
+//1614. Maximum Nesting Depth of the Parentheses[12-06-25]
+//3423. Maximum Difference Between Adjacent Elements in a Circular Array
 import java.util.*;
 
 public class week2 {
@@ -246,6 +247,62 @@ public int maxDepth(String s) {
         return max;     
     }
 
+    /*
+    3423. Maximum Difference Between Adjacent Elements in a Circular Array
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a circular array nums, find the maximum absolute difference between adjacent elements.
+
+Note: In a circular array, the first and last elements are adjacent.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,4]
+
+Output: 3
+
+Explanation:
+
+Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+
+Example 2:
+
+Input: nums = [-5,-10,-5]
+
+Output: 5
+
+Explanation:
+
+The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+
+ 
+
+Constraints:
+
+2 <= nums.length <= 100
+-100 <= nums[i] <= 100
+     */
+
+    public int maxAdjacentDistance(int[] nums) {
+        int max = 0;
+        int d;
+        for(int i=1;i<nums.length;i++){
+            d = nums[i]-nums[i-1];
+            d = Math.abs(d);
+            if(d>max){
+                max=d;
+            }
+        }
+        d = Math.abs(nums[nums.length-1]-nums[0]);
+        if(d>max) max=d;
+        return max;
+    }
     public static void main(String[] args) {
         // Example: Linked List = [4, 2, 1, 3]
         ListNode head = new ListNode(4);
