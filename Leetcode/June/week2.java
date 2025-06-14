b885760ef884f502a330f1fc8738126cf653f3f8
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-14 23:56:58 +0530

    2566. Maximum Difference by Remapping a Digit

--- Leetcode/June/week2.java
+++ Leetcode/June/week2.java
@@ -7,7 +7,7 @@
 // Game of Two Stacks
 //84. Largest Rectangle in Histogram[14-06-25]
 //921. Minimum Add to Make Parentheses Valid[14-06-25]
-
+//2566. Maximum Difference by Remapping a Digit[14-06-25]
 import java.util.*;
 
 public class week2 {
@@ -578,6 +578,69 @@ Output: 3
         return count;
     }
 
+/*
+2566. Maximum Difference by Remapping a Digit
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+
+Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+
+Notes:
+
+When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+Bob can remap a digit to itself, in which case num does not change.
+Bob can remap different digits for obtaining minimum and maximum values respectively.
+The resulting number after remapping can contain leading zeroes.
+ 
+
+Example 1:
+
+Input: num = 11891
+Output: 99009
+Explanation: 
+To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+The difference between these two numbers is 99009.
+Example 2:
+
+Input: num = 90
+Output: 99
+Explanation:
+The maximum value that can be returned by the function is 99 (if 0 is replaced by 9) and the minimum value that can be returned by the function is 0 (if 9 is replaced by 0).
+Thus, we return 99.
+ 
+
+Constraints:
+
+1 <= num <= 108
+ */
+    public int minMaxDifference(int num) {
+        String snum1 = Integer.toString(num);
+        String snum2=snum1;
+        int idx=0;
+        //maximum
+        for(int i=0;i<snum1.length();i++){
+            if(snum1.charAt(i)!='9'){
+                idx=i;
+                break;
+            }
+        }
+        if(idx<snum1.length()){
+            snum1 = snum1.replace(snum1.charAt(idx),'9');
+        }
+        //Minimum
+        
+        snum2 = snum2.replace(snum2.charAt(0),'0');
+
+        //Integer convert
+        return Integer.parseInt(snum1)-Integer.parseInt(snum2);
+
+    }
 
     public static void main(String[] args) {
         // Example: Linked List = [4, 2, 1, 3]
