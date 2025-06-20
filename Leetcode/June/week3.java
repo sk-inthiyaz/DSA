43ff87bd137d31c201e5677ca06b54ad1082a3c8
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-06-20 23:25:39 +0530

    901. Online Stock Span

--- Leetcode/June/week3.java
+++ Leetcode/June/week3.java
@@ -1,4 +1,5 @@
 // 1249. Minimum Remove to Make Valid Parentheses [18-06-2025]
+// 901. Online Stock Span [20-06-2025]
 class week3{
     /*
     1249. Minimum Remove to Make Valid Parentheses
@@ -62,4 +63,84 @@ s[i] is either '(' , ')', or lowercase English letter.
         // sn = sb.toString();
         return result.reverse().toString();
     }
+
+
+    /*
+    901. Online Stock Span
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+
+For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2, then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4 consecutive days.
+Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+ 
+
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+ 
+
+Constraints:
+
+1 <= price <= 105
+At most 104 calls will be made to next.
+*/
+
+/*
+class StockSpanner {
+    Stack<Integer> stack;//Each Element span,price
+    List<Integer> prices;
+    public StockSpanner() {
+        stack = new Stack<>();
+        prices = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        int i = prices.size();
+        prices.add(price);
+        while(stack.size()>0 && prices.get(stack.peek())<=price){
+            stack.pop();
+        }
+        int span;
+        if(stack.size()==0){
+            span = i + 1;
+        }else{
+            span = i - stack.peek();
+        }
+
+        stack.push(i);
+        return span;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+ */
+
+
 }
