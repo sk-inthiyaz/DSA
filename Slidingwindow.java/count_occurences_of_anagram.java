70788b0e205a64bddaf55889e66c0adc4d1667e8
sk-inthiyaz <skinthiyaz777@gmail.com>
2025-11-23 13:51:37 +0530

    Refactor count_occurences_of_anagram for clarity and remove unnecessary comments

--- Slidingwindow.java/count_occurences_of_anagram.java
+++ Slidingwindow.java/count_occurences_of_anagram.java
@@ -9,19 +9,32 @@ public class count_occurences_of_anagram {
         for(char c:ptr.toCharArray()){
             hm.put(c,hm.getOrDefault(c, 0)+1);
         }
+        
         int r=0,l=0,k=ptr.length(),count=hm.size(),ans=0;
         while(r<s.length()){
             char ch = s.charAt(r);
-            if(hm.containsKey(ch)) ;//decrese map value
-            if(hm.get(ch)==0) count--;
-            if(r-l+1<k) r++;
-            if(r-l+1==k){
-                //1.calculate answer 
-                //2.slide thew window
-                if(count==0) ans++;
+            
+            // Decrease map value if character exists
+            if(hm.containsKey(ch)) {
+                hm.put(ch, hm.get(ch) - 1);
+                if(hm.get(ch) == 0) count--;
             }
+            
+            // Window size reached
+            if(r-l+1 == k){
+                // Calculate answer
+                if(count == 0) ans++;
+                
+                // Slide the window - restore left character
+                char leftChar = s.charAt(l);
+                if(hm.containsKey(leftChar)) {
+                    if(hm.get(leftChar) == 0) count++;
+                    hm.put(leftChar, hm.get(leftChar) + 1);
+                }
+                l++;
+            }
+            r++;
         }
         System.out.println(ans);
     }
-    //
 }
